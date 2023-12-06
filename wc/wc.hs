4f693e4171640cb2f8e8a46ab8aa77835bb6659b
JVFeijo <joaovictor.feijo12@gmail.com>
2023-12-06 12:28:02 -0300

    add wc with the -c option

---
+++ wc/wc.hs
@@ -0,0 +1,33 @@
+import System.Environment
+import System.IO
+import Data.Char
+import qualified Data.ByteString.Lazy as BL
+import Data.Set (fromList, toList)
+import Control.Monad
+
+numberOfBytes :: BL.ByteString -> Integer
+numberOfBytes bs = (fromIntegral . BL.length) bs
+
+isValidOptionAux :: String -> Bool
+isValidOptionAux (c:cs) | c == 'c' = True
+                        | otherwise = False
+
+isValidOption :: String -> Bool
+isValidOption ('-':cs) = isValidOptionAux cs
+isValidOption _ = False
+
+removeHyphen :: String -> String
+removeHyphen str | isValidOption str == True = tail str
+                 | otherwise = str
+
+wc :: String -> BL.ByteString -> String
+wc [] bs = show (numberOfBytes bs)
+wc (c:cs) bs | c == 'c' = show (numberOfBytes bs)
+
+
+main = do
+         args <- getArgs
+         let fileName = head args
+         let options = (toList . fromList . mconcat . (map removeHyphen)) (tail args)
+         join $ putStrLn <$> (++ " " ++ fileName) <$> (wc options) <$> (BL.readFile fileName)
+          
