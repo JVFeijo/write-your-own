b74e99f62565daaa2854cbe799d2fc92304b9994
JVFeijo <joaovictor.feijo12@gmail.com>
2023-12-30 20:54:40 -0300

    add -m option that counts number of characters

--- wc/wc.hs
+++ wc/wc.hs
@@ -2,14 +2,14 @@
 import System.Environment
 import System.IO
 import Data.Char
---import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Char8 as BL
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.ByteString.Lazy.UTF8 as BLUTF8
 import Data.Set (fromList, toList)
 import Control.Monad
 import Control.Exception
 import Data.List
 
-data ValidOption = C | L | W deriving (Show)
+data ValidOption = C | L | W | M deriving (Show)
 
 data InvalidOptionException = IVE String deriving (Show)
 
@@ -32,6 +32,7 @@ isValidOption :: Char -> Either InvalidOptionException ValidOption
 isValidOption ch | ch == 'c' = Right C
                  | ch == 'l' = Right L
                  | ch == 'w' = Right W
+                 | ch == 'm' = Right M
                  | otherwise = Left (IVE "Invalid Option provided")
 
 numberOfBytes :: BL.ByteString -> Integer
@@ -43,10 +44,14 @@ numberOfLines = fromIntegral . (BL.count '\n')
 numberOfWords :: BL.ByteString -> Integer
 numberOfWords = fromIntegral . length . BL.words
 
+numberOfCharacters :: BL.ByteString -> Integer
+numberOfCharacters = fromIntegral . BLUTF8.length
+
 wc :: ValidOption -> BL.ByteString -> String
 wc C bs = show (numberOfBytes bs)
 wc L bs = show (numberOfLines bs)
 wc W bs = show (numberOfWords bs)
+wc M bs = show (numberOfCharacters bs)
 
 formatWCResult :: [String] -> String -> String
 formatWCResult strs fileName = unwords (strs ++ [fileName])
